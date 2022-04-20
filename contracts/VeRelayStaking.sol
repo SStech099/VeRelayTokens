4ed3ee00f2c87606346aa4730eedbcc2cbc50df0
SStech099 <99163081+SStech099@users.noreply.github.com>
2022-04-20 18:48:23 +0530

    Update VeRelayStaking.sol

--- contracts/VeRelayStaking.sol
+++ contracts/VeRelayStaking.sol
@@ -1,26 +1,26 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.7.6;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/SafeERC20Upgradeable.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "./VeRelayToken.sol";
 
-contract VeRelayStaking is OwnableUpgradeable {
-    using SafeMathUpgradeable for uint256;
-    using SafeERC20Upgradeable for IERC20Upgradeable;
+contract VeRelayStaking is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
     struct UserInfo {
         uint256 balance;
         uint256 rewardDebt;
         uint256 lastClaimTimestamp;
+        uint256 speedUpEndTimestamp;
     }
 
-    IERC20Upgradeable public relay;
+    IERC20 public relay;
     VeRelayToken public veRelay;
     uint256 public maxCapPct;
     uint256 public upperLimitMaxCapPct;
@@ -28,8 +28,11 @@ contract VeRelayStaking is OwnableUpgradeable {
     uint256 public ACC_VERELAY_PER_SHARE_PRECISION;
     uint256 public lastRewardTimestamp;
     uint256 public veRelayPerSharePerSec;
+    uint256 public speedUpVeRelayPerSharePerSec;
     uint256 public upperLimitVeRelayPerSharePerSec;
     uint256 public VERELAY_PER_SHARE_PER_SEC_PRECISION;
+    uint256 public speedUpThreshold;
+    uint256 public speedUpDuration;
 
     mapping(address => UserInfo) public userInfos;
 
@@ -37,15 +40,19 @@ contract VeRelayStaking is OwnableUpgradeable {
     event Deposit(address indexed user, uint256 amount);
     event UpdateMaxCapPct(address indexed user, uint256 maxCapPct);
     event UpdateRewardVars(uint256 lastRewardTimestamp, uint256 accVeRelayPerShare);
+    event UpdateSpeedUpThreshold(address indexed user, uint256 speedUpThreshold);
     event UpdateVeRelayPerSharePerSec(address indexed user, uint256 veRelayPerSharePerSec);
     event Withdraw(address indexed user, uint256 withdrawAmount, uint256 burnAmount);
 
     constructor(
-        IERC20Upgradeable _relay,
+        IERC20 _relay,
         VeRelayToken _veRelay,
         uint256 _veRelayPerSharePerSec,
+        uint256 _speedUpVeRelayPerSharePerSec,
+        uint256 _speedUpThreshold,
+        uint256 _speedUpDuration,
         uint256 _maxCapPct
-    ){
+    ) {
 
         require(address(_relay) != address(0), "VeRelayStaking: unexpected zero address for _relay");
         require(address(_veRelay) != address(0), "VeRelayStaking: unexpected zero address for _veRelay");
@@ -55,6 +62,17 @@ contract VeRelayStaking is OwnableUpgradeable {
             _veRelayPerSharePerSec <= upperLimitVeRelayPerSharePerSec,
             "VeRelayStaking: expected _veRelayPerSharePerSec to be <= 1e36"
         );
+        require(
+            _speedUpVeRelayPerSharePerSec <= upperLimitVeRelayPerSharePerSec,
+            "VeRelayStaking: expected _speedUpVeRelayPerSharePerSec to be <= 1e36"
+        );
+
+        require(
+            _speedUpThreshold != 0 && _speedUpThreshold <= 100,
+            "VeRelayStaking: expected _speedUpThreshold to be > 0 and <= 100"
+        );
+
+        require(_speedUpDuration <= 365 days, "VeRelayStaking: expected _speedUpDuration to be <= 365 days");
 
         upperLimitMaxCapPct = 10000000;
         require(
@@ -63,9 +81,12 @@ contract VeRelayStaking is OwnableUpgradeable {
         );
 
         maxCapPct = _maxCapPct;
+        speedUpThreshold = _speedUpThreshold;
+        speedUpDuration = _speedUpDuration;
         relay = _relay;
         veRelay = _veRelay;
         veRelayPerSharePerSec = _veRelayPerSharePerSec;
+        speedUpVeRelayPerSharePerSec = _speedUpVeRelayPerSharePerSec;
         lastRewardTimestamp = block.timestamp;
         ACC_VERELAY_PER_SHARE_PRECISION = 1e18;
         VERELAY_PER_SHARE_PER_SEC_PRECISION = 1e18;
@@ -91,6 +112,15 @@ contract VeRelayStaking is OwnableUpgradeable {
         emit UpdateVeRelayPerSharePerSec(_msgSender(), _veRelayPerSharePerSec);
     }
 
+    function setSpeedUpThreshold(uint256 _speedUpThreshold) external onlyOwner {
+        require(
+            _speedUpThreshold != 0 && _speedUpThreshold <= 100,
+            "VeRelayStaking: expected _speedUpThreshold to be > 0 and <= 100"
+        );
+        speedUpThreshold = _speedUpThreshold;
+        emit UpdateSpeedUpThreshold(_msgSender(), _speedUpThreshold);
+    }
+
     function deposit(uint256 _amount) external {
         require(_amount > 0, "VeRelayStaking: expected deposit amount to be greater than zero");
 
@@ -106,7 +136,11 @@ contract VeRelayStaking is OwnableUpgradeable {
 
             uint256 userStakedRelay = userInfo.balance;
 
+            if (_amount.mul(100) >= speedUpThreshold.mul(userStakedRelay)) {
+                userInfo.speedUpEndTimestamp = block.timestamp.add(speedUpDuration);
+            }
         } else {
+            userInfo.speedUpEndTimestamp = block.timestamp.add(speedUpDuration);
             userInfo.lastClaimTimestamp = block.timestamp;
         }
 
@@ -133,6 +167,7 @@ contract VeRelayStaking is OwnableUpgradeable {
         userInfo.balance = userInfo.balance.sub(_amount);
         userInfo.rewardDebt = accVeRelayPerShare.mul(userInfo.balance).div(ACC_VERELAY_PER_SHARE_PRECISION);
         userInfo.lastClaimTimestamp = block.timestamp;
+        userInfo.speedUpEndTimestamp = 0;
 
         uint256 userVeRelayBalance = veRelay.balanceOf(_msgSender());
         veRelay.burnFrom(_msgSender(), userVeRelayBalance);
@@ -167,13 +202,22 @@ contract VeRelayStaking is OwnableUpgradeable {
         uint256 pendingBaseVeRelay = _accVeRelayPerShare.mul(user.balance).div(ACC_VERELAY_PER_SHARE_PRECISION).sub(
             user.rewardDebt
         );
-      
+
+        uint256 pendingSpeedUpVeRelay;
+        if (user.speedUpEndTimestamp != 0) {
+            uint256 speedUpCeilingTimestamp = block.timestamp > user.speedUpEndTimestamp
+                ? user.speedUpEndTimestamp
+                : block.timestamp;
+            uint256 speedUpSecondsElapsed = speedUpCeilingTimestamp.sub(user.lastClaimTimestamp);
+            uint256 speedUpAccVeRelayPerShare = speedUpSecondsElapsed.mul(speedUpVeRelayPerSharePerSec);
+            pendingSpeedUpVeRelay = speedUpAccVeRelayPerShare.mul(user.balance).div(VERELAY_PER_SHARE_PER_SEC_PRECISION);
+        }
+
+        uint256 pendingVeRelay = pendingBaseVeRelay.add(pendingSpeedUpVeRelay);
 
         uint256 userVeRelayBalance = veRelay.balanceOf(_user);
 
         uint256 scaledUserMaxVeRelayCap = user.balance.mul(maxCapPct);
-
-        uint256 pendingVeRelay = pendingBaseVeRelay;
 
         if (userVeRelayBalance.mul(100) >= scaledUserMaxVeRelayCap) {
 
@@ -216,6 +260,10 @@ contract VeRelayStaking is OwnableUpgradeable {
         UserInfo storage userInfo = userInfos[_msgSender()];
 
         userInfo.rewardDebt = accVeRelayPerShare.mul(userInfo.balance).div(ACC_VERELAY_PER_SHARE_PRECISION);
+
+        if (userInfo.speedUpEndTimestamp != 0 && block.timestamp >= userInfo.speedUpEndTimestamp) {
+            userInfo.speedUpEndTimestamp = 0;
+        }
 
         if (veRelayToClaim > 0) {
             userInfo.lastClaimTimestamp = block.timestamp;
