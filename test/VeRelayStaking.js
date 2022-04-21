d9f22f0f8b775cb277e3f12490a8c189926fddd7
SStech099 <99163081+SStech099@users.noreply.github.com>
2022-04-21 19:18:36 +0530

    Update VeRelayStaking.js

--- test/VeRelayStaking.js
+++ test/VeRelayStaking.js
@@ -27,20 +27,25 @@ async function deploy() {
   await veToken.deployed();
 
   const VeRelayStaking = await ethers.getContractFactory("VeRelayStaking");
-  tokenStaking = await VeRelayStaking.deploy(token.address, veToken.address, 100, 100, 100, 300, 10000);
+  tokenStaking = await VeRelayStaking.deploy(token.address, veToken.address, 100, 100, 5, 300, 10000);
   await tokenStaking.deployed();
 
   await token.transfer(user1.address, 50000);
   await token.transfer(user2.address, 50000);
   await token.transfer(user3.address, 50000);
   await token.transfer(user4.address, 50000);
+
+  await token.connect(user1).approve(tokenStaking.address, 50000);
+  await token.connect(user2).approve(tokenStaking.address, 50000);
+  await token.connect(user3).approve(tokenStaking.address, 50000);
+  await token.connect(user4).approve(tokenStaking.address, 50000);
 }
 
-describe("Deploying Contracts", async() => {
-  it("Contracts Deployed", async() => {
+describe("Deploying Contracts", async function() {
+  it("Contracts Deployed", async function() {
       await getAddresses();
       await deploy();
-  }).timeout("150s");
+  }).timeout("2000s");
 });
 
 describe("Check deployments", function() {
@@ -53,13 +58,96 @@ describe("Check deployments", function() {
   });
   it("Should set the right state variables", async function() {
     expect(await tokenStaking.maxCapPct()).to.equal(10000);
-    expect(await tokenStaking.speedUpThreshold()).to.equal(100);
+    expect(await tokenStaking.speedUpThreshold()).to.equal(5);
     expect(await tokenStaking.speedUpDuration()).to.equal(300);
     expect(await tokenStaking.veRelayPerSharePerSec()).to.equal(100);
     expect(await tokenStaking.speedUpVeRelayPerSharePerSec()).to.equal(100);
   });
 });
 
-describe("Function testing", function() {
-  
+describe("Function setMaxCapPct", async function() {
+  it("Should be able to set macCapPct", async function() {
+    await tokenStaking.setMaxCapPct(12000);
+    expect(await tokenStaking.maxCapPct()).to.equal(12000);
+  });
+  it("Should fail if non owner tries to set set maxCapPct", async function() {
+    await expect(tokenStaking.connect(user1).setMaxCapPct(12000)).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+  it("Should not allow owner to set less maxCapPct value than the current one", async function() {
+    await expect(tokenStaking.setMaxCapPct(9000)).to.be.revertedWith("VeRelayStaking: expected new _maxCapPct to be greater than existing maxCapPct");
+  });
+  it("Should not allow owner to set greater maxCapPct value than upper limit", async function() {
+    await expect(tokenStaking.setMaxCapPct(100000000)).to.be.revertedWith("VeRelayStaking: expected new _maxCapPct to be non-zero and <= 10000000");
+  });
+});
+
+describe("Function setVeRelayPerSharePerSec", async function() {
+  it("Should be able to set VeRelayPerSharePerSec", async function() {
+    await tokenStaking.setVeRelayPerSharePerSec(1000);
+    expect(await tokenStaking.veRelayPerSharePerSec()).to.equal(1000);
+  });
+  it("Should fail if non owner tries to set set VeRelayPerSharePerSec", async function() {
+    await expect(tokenStaking.connect(user1).setVeRelayPerSharePerSec(1000)).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+  it("Should not allow owner to set less maxCapPct value than the current one", async function() {
+    await expect(tokenStaking.setMaxCapPct(9000)).to.be.revertedWith("VeRelayStaking: expected new _maxCapPct to be greater than existing maxCapPct");
+  });
+  it("Should not allow owner to set greater maxCapPct value than upper limit", async function() {
+    await expect(tokenStaking.setMaxCapPct(100000000)).to.be.revertedWith("VeRelayStaking: expected new _maxCapPct to be non-zero and <= 10000000");
+  });
+});
+
+describe("Function setSpeedUpThreshold", async function() {
+  it("Should be able to set SpeedUpThreshold", async function() {
+    await tokenStaking.setSpeedUpThreshold(10);
+    expect(await tokenStaking.speedUpThreshold()).to.equal(10);
+  });
+  it("Should fail if non owner tries to set set SpeedUpThreshold", async function() {
+    await expect(tokenStaking.connect(user1).setSpeedUpThreshold(10)).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+  it("Should not allow owner to set SpeedUpThreshold greater than 100", async function() {
+    await expect(tokenStaking.setSpeedUpThreshold(110)).to.be.revertedWith("VeRelayStaking: expected _speedUpThreshold to be > 0 and <= 100");
+  });
+  it("Should not allow owner to set SpeedUpThreshold equal to 0", async function() {
+    await expect(tokenStaking.setSpeedUpThreshold(0)).to.be.revertedWith("VeRelayStaking: expected _speedUpThreshold to be > 0 and <= 100");
+  });
+});
+
+describe("Function Deposit", function() {
+
+  it("Should update the variables after deposit", async function() {
+    const beforeUser1Info = await tokenStaking.userInfos(
+      user1.address
+    );
+    expect(beforeUser1Info[0]).to.equal(0);
+    expect(beforeUser1Info[1]).to.equal(0);
+    expect(beforeUser1Info[2]).to.equal(0);
+    expect(beforeUser1Info[3]).to.equal(0);
+
+    // Check joe balance before deposit
+    expect(await token.balanceOf(user1.address)).to.equal(50000);
+
+    await tokenStaking.connect(user1).deposit(5000);
+    const depositBlock = await ethers.provider.getBlock();
+
+    // Check joe balance after deposit
+    expect(await token.balanceOf(user1.address)).to.equal(45000);
+
+    const afterUser1Info = await tokenStaking.userInfos(
+      user1.address
+    );
+    expect(afterUser1Info[0]).to.equal(5000);
+    expect(afterUser1Info[1]).to.equal(0);
+    expect(afterUser1Info[2]).to.equal(depositBlock.timestamp);
+    expect(afterUser1Info[3]).to.equal(
+      depositBlock.timestamp + tokenStaking.speedUpDuration);
+  });
+});
+
+describe("function Withdraw", function () {
+  it("Should not execute if the withdraw amount is zero", async function() {
+    expect(await tokenStaking.withdraw(0),{
+      gasLimit: 10000000,
+  }).to.be.revertedWith("VeRelayStaking: expected withdraw amount to be greater than zero");
+  });
 });
